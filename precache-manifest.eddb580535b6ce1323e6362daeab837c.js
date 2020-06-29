94e3fd5122dd967f937ee500ea05b3f1569b4e76
shaunstone0 <shaunstone0@gmail.com>
2020-06-29 09:17:35 +0200

    Updates

---
+++ precache-manifest.eddb580535b6ce1323e6362daeab837c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d0861cb0c6f02b9946f22ffbe3b8534",
+    "url": "/Dashboard-Firebase/index.html"
+  },
+  {
+    "revision": "b43f7c63620281005a96",
+    "url": "/Dashboard-Firebase/static/css/main.5c5e5f9e.chunk.css"
+  },
+  {
+    "revision": "f3b6c39295c7fc99d328",
+    "url": "/Dashboard-Firebase/static/js/2.6b79c7eb.chunk.js"
+  },
+  {
+    "revision": "4224b32d50aca7d0b4f248bf0384ed4f",
+    "url": "/Dashboard-Firebase/static/js/2.6b79c7eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b43f7c63620281005a96",
+    "url": "/Dashboard-Firebase/static/js/main.6a946ef6.chunk.js"
+  },
+  {
+    "revision": "1c4b9a83fc1836b38bc3",
+    "url": "/Dashboard-Firebase/static/js/runtime-main.ab830dcd.js"
+  },
+  {
+    "revision": "701d76c0fe0255b7ae548df8241fe6bd",
+    "url": "/Dashboard-Firebase/static/media/numbers-background.701d76c0.jpg"
+  }
+]);
